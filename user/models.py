15e15297b5b323f2f1b8a8a467184473ecb40e0a
Agoin <254327863@qq.com>
2021-09-17 09:49:57 -0700

    create user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    """用户"""
+    username = models.CharField(u'用户名', max_length=50, null=True)
+    password = models.CharField(u'密码', max_length=50)
+    phone = models.CharField(u'手机号', max_length=32, unique=True, null=True)
+    country_id = models.IntegerField(u"国家id", null=False, default=0)
+    nickname = models.CharField(u'昵称', max_length=50, null=True)
+    email = models.EmailField(u'邮箱地址', max_length=120, null=True)
+    create_time = models.IntegerField(u'创建时间')
+    update_time = models.IntegerField(u'更新时间')
--- wallet/urls.py
+++ wallet/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include(user.urls))
+    # path('api/user/', include('user.urls'))
 ]
